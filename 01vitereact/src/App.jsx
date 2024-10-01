e6f9334df6e937b3042bfcf7a7403ae45964d3f7
Mayursingh9111 <mayursingh9112001@gmail.com>
2024-10-02 01:22:18 +0530

    DAY1

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+function App() {
+
+  return (
+    // <h1>Chai aur React with Vite | Mayur Singh Shekhawat</h1>
+    <>
+    <h1>Chai aur React with Vite | Mayur Singh Shekhawat</h1>
+    <Chai />
+    </>
+  )
+}
+
+export default App
